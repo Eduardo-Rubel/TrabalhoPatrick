64773407399b8bb3fd1b6acc0fdf0d3be21f7c6a
Adriel <70505492+adMetalico@users.noreply.github.com>
2024-09-24 11:38:54 -0300

    Add files via upload

---
+++ cardapio.java
@@ -0,0 +1,26 @@
+package fag;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class cardapio {
+    private List<pratos> cardapio;
+
+    public cardapio() {this.cardapio = new ArrayList<pratos>();}
+
+    public void adicionarprato (pratos prato) {
+        this.cardapio.add(prato);
+    }
+
+    public void removerprato (int codigodoprato) {
+        boolean removido = false;
+        List<pratos> pratospararemover = new ArrayList<>();
+        for (pratos p : this.cardapio) {
+            if (p.getCodigodoprato()==codigodoprato) {
+                pratospararemover.add(p);
+                removido = true;
+            }
+        }
+        cardapio.removeAll(pratospararemover);
+    }
+}
